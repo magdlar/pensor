623bc4efc431ce254b7d80e42d1c787c70e9ce62
magdlar <magnusdlarsen@gmail.com>
2020-03-06 20:21:31 +0100

    shiny skeleton

--- frontend/shinyserver.R
+++ frontend/shinyserver.R
@@ -5,24 +5,41 @@ library(tidyverse)
 
 #getOption(shiny.port = "128.39.248.79")
 
-ui <- fluidPage(
-  
-  titlePanel("Pensor"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      fileInput("file", "Velg pensum",
-                multiple = FALSE,
-                accept = c("text/pdf",
-                           #"text/comma-separated-values,text/plain",
-                           ".pdf"))
-    ),
-  mainPanel("Her skal det skje ting.")
-  )
-)
+# ui <- fluidPage(
+#   
+#   titlePanel("Pensor"),
+#   
+#   sidebarLayout(
+#     sidebarPanel(
+#       fileInput("file", "Velg pensum",
+#                 multiple = FALSE,
+#                 accept = c("text/pdf",
+#                            #"text/comma-separated-values,text/plain",
+#                            ".pdf"))
+#     ),
+#   mainPanel("Her skal det skje ting.")
+#   )
+# )
 
+ui <- navbarPage("Pensor",
+  navbarMenu("=",
+             tabPanel("Home",
+                      verbatimTextOutput("txtout")
+                      ),
+            tabPanel("Upload",
+                     fileInput("file", "Last opp pensum",
+                               multiple = FALSE,
+                               accept = c("text/pdf",
+                                          #"text/comma-separated-values,text/plain",
+                                          ".pdf")
+                               )
+                     )
+            )
+  )
 server <- function(input, output) {
-  
+  output$txtout <- renderText({
+    "just some text"
+  })
 }
 
 shinyApp(ui, server)
